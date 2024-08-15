4220bea11d7a1b5dfa9e8e231367f4ff1126938a
Maverick Wong <mvrck.wong@gmail.com>
2024-08-15 15:01:30 +0800

    update

--- src/config_db.py
+++ src/config_db.py
@@ -1,19 +1,17 @@
 from os import getenv
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
-from typing import Final
-
 from config_path import PROJECT_PATH, ENV_PATH
 
 
-# Standard configuration 
-env_file = ENV_PATH / '.env'
-if not env_file.exists():
-    raise FileNotFoundError(f"Environment file {env_file} not found!")
-
 def db_connection():
     """ Establish a connection to a PostgreSQL database using the provided connection URL. """
-    load_dotenv(env_file)
+    # Standard configuration 
+    env_file = ENV_PATH / '.env'
+    if not env_file.exists():
+        raise FileNotFoundError(f"Environment file {env_file} not found!")
+    else:
+        load_dotenv(env_file)
     
     db_creds_var = ['DB_USER', 'DB_PW', 'DB_HOST', 'DB_NAME']
     for var in db_creds_var:
@@ -24,7 +22,9 @@ def db_connection():
     connect_url = f"postgresql://{getenv('DB_USER')}:{getenv('DB_PW')}@{getenv('DB_HOST')}/{getenv('DB_NAME')}?sslmode=require"
     engine = create_engine(connect_url)
     return engine
+ 
 
-
+# Running the main function
 if __name__ == "__main__":
+    print(db_connection())
     None
